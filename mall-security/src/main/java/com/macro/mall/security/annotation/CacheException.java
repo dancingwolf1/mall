e10e353452ddbc021269925ab72c7ce474ba871f
18790252057 <1025857155@qq.com>
2020-06-23 10:31:44 +0800

    自定义注解

--- mall-security/src/main/java/com/macro/mall/security/annotation/CacheException.java
+++ mall-security/src/main/java/com/macro/mall/security/annotation/CacheException.java
@@ -6,7 +6,7 @@ import java.lang.annotation.*;
  * 自定义注解，有该注解的缓存方法会抛出异常
  */
 @Documented
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)// 可能出现在Java程序中的语法位置
+@Retention(RetentionPolicy.RUNTIME)//描述保留注解的各种策略，它们与元注解(@Retention)一起指定注释要保留多长时间
 public @interface CacheException {
 }
